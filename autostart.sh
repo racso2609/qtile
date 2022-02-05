4a9dce2fc4bcbdae70ae07cea572f6dd90d4a54d
Oscar <oscaremiliolugo@gmail.com>
2022-02-04 23:05:38 -0500

    update personal config

--- autostart.sh
+++
@@ -1,2 +0,0 @@
-cbatticon &
-notify-send "Puticaaaa"
--- config.py
+++ config.py
@@ -10,14 +10,34 @@ from settings.layouts import layouts, floating_layout
 from settings.mouse import mouse
 from settings.screen import screens
 
+# from libqtile.log_utils import logger
 
+# @hook.subscribe.startup_once
+# def autostart():
+    # subprocess.call([path.join(path.expanduser('~'),'.config','qtile', 'autostart.sh')])
+
+
+
+# subprocess.call(['~/.config/qtile/scripts/autostart.sh'])
+
+main = None
 @hook.subscribe.startup_once
-def autostart():
-    subprocess.call([path.join(path.expanduser('~'),'.config','qtile', 'autostart.sh')])
+def start_once():
+    home = os.path.expanduser('~/.config/qtile/scripts/autostart.sh')                            
+    subprocess.call([home])
 
+@hook.subscribe.startup
+def start_always():
+    # Set the cursor to something sane in X
+    subprocess.Popen(['xsetroot', '-cursor_name', 'left_ptr'])
 
+@hook.subscribe.client_new
+def set_floating(window):
+    if (window.window.get_wm_transient_for()
+            or window.window.get_wm_type() in floating_types):
+        window.floating = True
 
-
+floating_types = ["notification", "toolbar", "splash", "dialog"]
 
 dgroups_key_binder = None
 dgroups_app_rules = []  # type: List
@@ -27,9 +47,6 @@ cursor_warp = True
 auto_fullscreen = True
 focus_on_window_activation = "smart"
 reconfigure_screens = True
-
-
-
 
 auto_minimize = True
 
