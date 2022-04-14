47a7808fe13b930749e86c7c772aa010fdc62d61
oscar lugo <oscaremiliolugo@gmail.com>
2022-04-14 23:04:06 +0000

    fix bar layout

--- settings/screen.py
+++ settings/screen.py
@@ -6,6 +6,7 @@ def init_screens():
     return [
         Screen(
             # top=bar.Bar(widgets=widgets_screen1, size=26, opacity=0.8)),
+            ),
         Screen(
             top=bar.Bar(widgets=widgets_screen2, size=26, opacity=0.8))
     ]
